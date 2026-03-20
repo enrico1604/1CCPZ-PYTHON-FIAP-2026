85fbfe64909eaea70ab6b9b041932c19434d1896
enrico1604 <enricomarinhodeaquino10@gmail.com>
2026-03-20 20:50:12 -0300

    aula 2 variaveis e operadores + desafios

---
+++ Desafios variaveis aula 2.py
@@ -0,0 +1,16 @@
+nome = input ('digite seu nome:')
+print (f"bem vindo,{nome}!!")  #desafio 1
+
+dia = input  ("digite o dia do seu nascimento:")
+mes = input  ("digite o mes do seu nascimento:")
+ano = input  ("digite o ano do seu nascimento:")  #desafio 2
+
+data_nasc = dia + "/" + mes + "/" + ano
+print (data_nasc)
+
+valor1 = int(input ("digite o valor 1:"))
+valor2 = int(input ("digite o valor 2:"))  #desafio 3
+
+valor_soma = valor1 + valor2
+print (f"a soma é {valor_soma}")
+
---
+++ aula 02 variaveis e operadores/aula 02 variaveis.py
@@ -0,0 +1,33 @@
+#variaveis
+
+nome = 'enrico' #string
+idade = 17 #int (variavel numerica com numerous interops)
+peso = 73 #float (variavel numerica com decimals)
+
+print (nome, idade, peso)
+
+print(f"oi, {nome}!") #o f chama a variavel
+
+#imput simula um forms no cmd
+
+nome = input ("qual o seu nome:")
+idade =int (input ('digite sua idade:'))
+nova_idade = idade +1
+print (nome, idade)
+print (nova_idade)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
